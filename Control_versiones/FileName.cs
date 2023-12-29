08cb55b0d6db8816294a851eb818d11239744e21
John <johnale22@outlook.com>
2023-12-28 19:37:22 -0500

    control versiones

---
+++ Control_versiones/FileName.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_versiones
+{
+    internal class FileName
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***   Ejemplo de proyecto   ***");
+            
+
+            string text = @"   __        _               ___                           
+   \ \  ___ | |__  _ __     / _ \_   _  ___ _ __ _ __ __ _ 
+    \ \/ _ \| '_ \| '_ \   / /_\/ | | |/ _ \ '__| '__/ _` |
+ /\_/ / (_) | | | | | | | / /_\\| |_| |  __/ |  | | | (_| |
+ \___/ \___/|_| |_|_| |_| \____/ \__,_|\___|_|  |_|  \__,_|
+                                                           ";
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
+
